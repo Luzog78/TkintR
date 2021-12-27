686d1c14b60728684ecbe30a2546f6f5952be29f
Luzog78 <sabik.yanis@gmail.com>
2021-12-27 14:15:55 +0100

    Initial Configuration

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ from tkinter import *
 
 tk = Tk()
 
+tk.title("Mon Application Tkinter")
+tk.geometry("720x480")
+tk.iconbitmap("src/icon.ico")
+tk.configure(background="gray", highlightthickness=16)
+
 tk.mainloop()
